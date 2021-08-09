b938460c1b0c5bbb644e40b5ea6bbc567c474ca5
jonatanfrederiksen <Jonatan3230@gmail.com>
2021-08-09 11:14:20 +0200

    bug fixes for safari & firefox

--- menu_button.js
+++ menu_button.js
@@ -6,4 +6,5 @@ function toggleNav() {
     mobilemenu.classList.toggle('open_nav')
     mobilemenu.classList.toggle('close_nav')
 }
+
 document.getElementById('menu_open_btn').addEventListener('click', toggleNav);
